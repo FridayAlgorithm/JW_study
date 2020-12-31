bf02326e72264354cc24f16e8d3d9989d8302d75
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-01 01:50:03 +0900

    Create 2798.py

---
+++ Baekjoon/bruteforce/2798.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+x = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            if l[i] + l[j] + l[k] >= x and l[i] + l[j] + l[k] <= m:
+                x = l[i] + l[j] + l[k]
+print(x)
