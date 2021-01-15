654298933fc0d819060398309eeb8aa973bda617
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-15 16:27:56 +0900

    Update 2164.py

--- Baekjoon/queue/2164.py
+++ Baekjoon/queue/2164.py
@@ -1,26 +1,15 @@
 from collections import deque
 
-from collections import deque
-n = int(input())
-nums = deque()
+N = int(input())
+deque = deque([i for i in range(1, N + 1)])
 
-for i in range(1, n+1):
-    nums.append(i)
-
-
-def que():
-    cnt = 0
-    while(len(nums) >= 1):
-        cnt += 1
-        if(len(nums) == 1):
-            return nums[0]
-
-        if(cnt % 2 == 1):
-            nums.popleft()
-
-        elif(cnt % 2 == 0):
-            nums.append(nums.popleft())
-
-
-print(que())
+while(not (len(deque) == 1)):
+    deque.popleft()
+    move = deque.popleft()
+    deque.append(move)
+    
+print(deque[0])
+#디큐 정의 확인
+#popleft() pop()을 하되 왼쪽부터 값을 뺀다. 
+#버리고 맨앞의 숫자를 뒤로 옮기기만 잘해주면 되는 문제
 #디큐안쓰면 카드배열 생성하고 하는데서 시간초과오류 오지게뜸
