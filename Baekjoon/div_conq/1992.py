e4483de04429cc91e28544c30aaa0afc9226c56e
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-15 16:54:43 +0900

    Update 1992.py

--- Baekjoon/div_conq/1992.py
+++ Baekjoon/div_conq/1992.py
@@ -1,6 +1,6 @@
 n = int(input()) 
 video = [list(input()) for _ in range(n)] 
-def divide(x, y, n): 
+def div(x, y, n): 
   ans = [] 
   check = True 
   color = video[x][y] 
@@ -12,12 +12,12 @@ def divide(x, y, n):
       if color != video[i][j]: 
         check = False 
         ans.append("(") 
-        ans.extend(divide(x, y, n//2)) 
-        ans.extend(divide(x, y+n//2, n//2)) 
-        ans.extend(divide(x+n//2, y, n//2)) 
-        ans.extend(divide(x+n//2, y+n//2, n//2))
+        ans.extend(div(x, y, n//2)) 
+        ans.extend(div(x, y+n//2, n//2)) 
+        ans.extend(div(x+n//2, y, n//2)) 
+        ans.extend(div(x+n//2, y+n//2, n//2))
         ans.append(")") 
         return ans 
    return color 
      
-print("".join(divide(0, 0, n)))
+print("".join(div(0, 0, n)))
