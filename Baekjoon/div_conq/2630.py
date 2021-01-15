81d839e605d42785bc433e6e48ad9350ad1944ef
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-15 16:52:16 +0900

    Update 2630.py

--- Baekjoon/div_conq/2630.py
+++ Baekjoon/div_conq/2630.py
@@ -1,31 +1,32 @@
-N = int(input())
-paper = [list(map(int, input().split())) for _ in range(N)]
-
-white, blue = 0, 0 
-
-def divide(x, y, n):
-  global white, blue 
-  color = paper[x][y] 
-  check = True 
-  
-  for i in range(x, x+n): 
-    if not check: 
-      break 
-      
-    for j in range(y, y+n): 
-      if color != paper[i][j]: 
-        check = False 
-        divide(x, y, n//2)
-        divide(x, y+n//2, n//2)  
-        divide(x+n//2, y, n//2) 
-        divide(x+n//2, y+n//2, n//2) 
-        break 
-        
-     if check:
-      if color:
-        blue += 1 
-      else: 
-        white += 1 
-divide(0, 0, N) 
-print(white) 
+import sys
+n=int(sys.stdin.readline())
+ 
+color_paper=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]#x행 y열
+ 
+white=0
+blue=0
+ 
+def div(x,y,n):
+    global blue,white
+    check=color_paper[x][y]
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if check!=color_paper[i][j]:
+                div(x,y,n//2)
+                div(x,y+n//2,n//2)
+                div(x+n//2,y,n//2)
+                div(x+n//2,y+n//2,n//2)
+                return
+ 
+ 
+    if check==0:
+        white+=1
+        return
+    else:   
+        blue+=1
+        return
+ 
+ 
+div(0,0,n)
+print(white)
 print(blue)
