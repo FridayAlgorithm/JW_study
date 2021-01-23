4ddce5aba6621dbf564e5d3f025f23a891a4da7c
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-24 02:37:48 +0900

    Create 2606.py

---
+++ Baekjoon/DFS&BFS/2606.py
@@ -0,0 +1,20 @@
+n = int(input())
+t = int(input())
+s = [[0] * n for i in range(n)]
+visit = [0 for i in range(n)]
+for i in range(t):
+    a, b = map(int, input().split())
+    s[a - 1][b - 1] = 1
+    s[b - 1][a - 1] = 1
+def dfs(v):
+    print(v, end = ' ')
+    visited[v] = True
+    for e in arr[v]:
+        if visited[e] == False:
+            dfs(e)
+dfs(0)
+cnt = 0
+for i in range(1, n):
+    if visit[i] == 1:
+        cnt += 1
+print(cnt)
