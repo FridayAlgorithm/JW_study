7793e267983d14762b6af185bc01023db68cc820
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-24 14:21:17 +0900

    Update 2447.py

--- Baekjoon/recursion/2447.py
+++ Baekjoon/recursion/2447.py
@@ -2,9 +2,11 @@ import sys
 
 def draw(x,y):
     while x != 0:
+         # 몫이 1인 경우
         if (x%3) == 1 and (y%3) == 1:
             sys.stdout.write(' ')
             return
+        # 3으로 나누어서 위의 if문에 걸리면 그 부분도 빈칸 처리
         x = x // 3
         y = y // 3
 
