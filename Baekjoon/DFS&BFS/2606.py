78fd8e1649b4e025b1b5cd62be0c1459b66f0095
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-24 14:50:16 +0900

    Update 2606.py

--- Baekjoon/DFS&BFS/2606.py
+++ Baekjoon/DFS&BFS/2606.py
@@ -1,7 +1,7 @@
 n = int(input())
 t = int(input())
 s = [[0] * n for i in range(n)]
-visit = [0 for i in range(n)]
+visited = [0 for i in range(n)]
 for i in range(t):
     a, b = map(int, input().split())
     s[a - 1][b - 1] = 1
@@ -10,11 +10,11 @@ def dfs(v):
     #print(v, end = ' ')
     visited[v] = True
     for e in range(n):
-        if s[v][e] == 1 and if visited[e] == False:
+        if s[v][e] == 1 and visited[e] == False:
             dfs(e)
 dfs(0)
 cnt = 0
 for i in range(1, n):
-    if visit[i] == 1:
+    if visited[i] == 1:
         cnt += 1
 print(cnt)
