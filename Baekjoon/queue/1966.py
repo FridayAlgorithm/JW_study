3928b37200d8c7c08de88c6d3b310e724bb758ca
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-01-29 03:14:51 +0900

    Update 1966.py

--- Baekjoon/queue/1966.py
+++ Baekjoon/queue/1966.py
@@ -10,11 +10,11 @@ for _ in range(test_cases):
     order = 0
     
     while True:
-        # 첫번째 if: imp의 첫번째 값 = 최댓값?
+        # 첫번째 if: imp의 첫번째 값 = 최댓값
         if imp[0]==max(imp):
             order += 1
                         
-            # 두번째 if: idx의 첫 번째 값 = "target"?
+            # 두번째 if: idx의 첫 번째 값 = "target"
             if idx[0]=='target':
                 print(order)
                 break
