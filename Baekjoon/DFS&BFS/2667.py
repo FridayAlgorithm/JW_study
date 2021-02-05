8fa9855adabcfb35b85303b6cd7a16e3961d491c
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-02-05 14:48:23 +0900

    Update 2667.py

--- Baekjoon/DFS&BFS/2667.py
+++ Baekjoon/DFS&BFS/2667.py
@@ -2,6 +2,7 @@ n = int(input())
 s = []
 horiz = [1, -1, 0, 0]
 verti = [0, 0, -1, 1]
+//가로세로 탐색해나갈때 쓰일 좌표용
 cnt = []
 for i in range(n):
     s.append(list(input()))
@@ -11,14 +12,17 @@ def bfs(i, j):
     count = 1
     while queue:
         a, b = queue[0][0], queue[0][1]
+        //오른쪽 아래쪽으로 탐색
         del queue[0]
         for k in range(4):
             x = a + horiz[k]
             y = b + verti[k]
             if 0 <= x < n and 0 <= y < n and s[x][y] == "1":
+                //찾았다
                 s[x][y] = "0"
                 queue.append([x, y])
                 count += 1
+                //갯수증가
     cnt.append(count)
 for i in range(n):
     for j in range(n):
