7dc4b1cba9aa6943e0223bd26a06a023cf5798ab
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-02-05 16:19:56 +0900

    Create 1780.py

---
+++ Baekjoon/div_conq/1780.py
@@ -0,0 +1,34 @@
+def check(x,y,n):
+    global cnt_zero,cnt_one,cnt_minus
+    tmp = s[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if tmp != s[i][j]:
+                k = n//3
+                check(x,y,k)
+                check(x,y+k,k)
+                check(x,y+2*k,k)
+                check(x+k,y+2*k,k)
+                check(x+k,y,k)
+                check(x+2*k,y,k)
+                check(x+2*k,y+k,k)
+                check(x+k,y+k,k)
+                check(x+2*k,y+2*k,k)
+                return
+    if tmp == 0:
+        cnt_zero += 1
+    elif tmp == 1:
+        cnt_one += 1
+    else:
+        cnt_minus += 1
+
+
+n = int(input())
+s = []
+cnt_zero, cnt_one, cnt_minus = 0, 0, 0
+for _ in range(n):
+    s.append(list(map(int, input().split( ))))
+check(0,0,n)
+print(cnt_minus)
+print(cnt_zero)
+print(cnt_one)
