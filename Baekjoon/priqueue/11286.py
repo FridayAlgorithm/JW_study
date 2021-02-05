a5a8e058e2f1440f9f9898a3235fe37853db8cbf
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-02-05 16:12:59 +0900

    Update 11286.py

--- Baekjoon/priqueue/11286.py
+++ Baekjoon/priqueue/11286.py
@@ -5,8 +5,12 @@ for _ in range(int(input())):
     num = int(sys.stdin.readline())
     if num != 0:
         heapq.heappush(heap,(abs(num),num))
+                //heappush ->  힙에 원소를 추가하기 (원소를 추가할 대상 리스트,추가할 원소)
+
     else:
         if heap:
             print(heapq.heappop(heap)[1])
+                    //heappop -> 원소를 삭제할 대상 리스트를 인자로 넘기면, 가장 작은 원소를 삭제 후에 그 값을 리턴
+
         else:
             print(0)
