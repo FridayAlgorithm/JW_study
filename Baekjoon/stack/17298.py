2569c5958a0731c9ee0b97bbcd2fff5917a5f06a
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-02-19 00:42:44 +0900

    Update 17298.py

--- Baekjoon/stack/17298.py
+++ Baekjoon/stack/17298.py
@@ -1,15 +1,17 @@
 num = int(input())
-a = list(map(int, input().split(" ")))
-result = ["-1" for _ in range(num)]
+sequence = list(map(int, input().split(" ")))
+result = [-1 for _ in range(num)]
 stack = []
 stack.append(0)
 i = 1
 for i in range(num):
-    while stack and a[stack[-1]] < a[i]:
-        result[stack[-1]] = str(a[i])
+    while stack and sequence[stack[-1]] < sequence[i]:
+        result[stack[-1]] = str(sequence[i])
         stack.pop()
 
     stack.append(i)
     i += 1
 
 print(" ".join(result))
+
+runtimeerror ㄷ ㄷ ㄷ 
