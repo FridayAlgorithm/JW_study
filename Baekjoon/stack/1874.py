dc021098ccbdfcf303a55a376bc965ae392938d3
carpediemstarshark <57650630+carpediemstarshark@users.noreply.github.com>
2021-02-18 23:49:57 +0900

    Create 1874.py

---
+++ Baekjoon/stack/1874.py
@@ -0,0 +1,22 @@
+//문제의도파악못해서 클론코딩 공부필요.
+n = int(input())
+s = []
+op = []
+count = 1
+temp = True
+for i in range(n):
+    num = int(input())
+    while count <= num:
+        s.append(count)
+        op.append('+')
+        count += 1
+    if s[-1] == num:
+        s.pop()
+        op.append('-')
+    else:
+        temp = False
+if temp == False:
+    print('NO')
+else:
+    for i in op:
+        print(i)
